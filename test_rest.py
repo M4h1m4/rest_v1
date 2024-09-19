7727970566477cb944c8540d302bd16cdb8c1370
Mahima <61741857+M4h1m4@users.noreply.github.com>
2024-09-19 07:41:40 +0530

    Update test_rest.py

--- test_rest.py
+++ test_rest.py
@@ -3,6 +3,6 @@ import rest_api_server
 import math
 
 def test_area_circle():
-    area = algebra.area_circle(100)
-    assert math.isclose(area, 31415.926535897932)
+    area = algebra.area_circle(10)
+    assert math.isclose(area, 314.15926535897932)
 
